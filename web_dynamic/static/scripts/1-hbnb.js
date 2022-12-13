c0140f5f362209f65409de24cf7d3dea936325b5
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-12-12 22:02:15 -0600

    Edit: fixed logo in 3-header.css, added caching in all templates.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,14 @@
-#!/usr/bin/node
+$(document).ready(function (){
+    let amenityObj = [];
 
-function init () {
-  const amenityObj = {};
-  ('.amenities .popover input').change(function () {
-    if ((this).is(':checked')) {
-      amenityObj[(this).attr('data-name')](this).attr('data-id');
-    } else if ((this).is(':not(:checked')) {
-      delete amenityObj[(this).attr('data-name')];
-    }
-    const names = Object.keys(amenityObj);
-    ('.amenities h4').text(names.sort().join(', '));
-  });
-}
+    $('li :checkbox').change(function () {
+	const item = $(this).attr('data-name');
+
+	if (this.checked) {
+            amenityObj.push(item);
+	} else {
+	    amenityObj = amenityObj.filter((item) => item !== dataName);
+	}
+	$('div.amenities h4').text(amenityObj.join(', '));
+    });
+});
